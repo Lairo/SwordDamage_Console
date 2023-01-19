07378a6ba74756ee51d2b691be80a82becc5f1a3
chajus1 <chajus1@googlemail.com>
2023-01-19 08:04:51 +0000

    Refactoring Encapsulation of the SwordDamage.cs

--- SwordDamage_Console/Program.cs
+++ SwordDamage_Console/Program.cs
@@ -16,7 +16,7 @@ namespace SwordDamage_Console
             SwordDamage swordDamage = new SwordDamage();
             while (true)
             {
-                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming +" +
+                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming " +
                     "3 for both, anything else to quit:" );
                 char key = Console.ReadKey().KeyChar;
                 if (key != '0' && key != '1' && key != '2' && key != '3') return;
@@ -28,54 +28,5 @@ namespace SwordDamage_Console
             }
 
         }
-
-        //My solution
-        //public static SwordDamage sword = new SwordDamage();
-        //public static Random randomizer = new Random();
-
-        //static void Main(string[] args)
-        //{
-        //    int[] DiceRolls = new int[3];
-        //    while (true)
-        //    {
-        //        Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming 3 for both, anything else to quit: ");
-        //        char input = Console.ReadKey(false).KeyChar;
-        //        Console.Write("\n");
-
-        //        DiceRolls[0] = randomizer.Next(1, 7);
-        //        DiceRolls[1] = randomizer.Next(1, 7);
-        //        DiceRolls[2] = randomizer.Next(1, 7);
-        //        sword.Roll = DiceRolls.Sum();
-
-        //        if (input == '0')
-        //        {
-        //            Prompt();
-        //        }
-        //        else if (input == '1' || input == '3')
-        //        {
-        //            sword.SetMagic(true);
-        //            Prompt();
-        //        }
-
-        //        else if (input == '2' || input == '3')
-        //        {
-        //            sword.SetFlaming(true);
-        //            Prompt();
-        //        }
-        //        else
-        //        {
-        //            //sword.SetFlaming(false);
-        //            //sword.SetMagic(false);
-        //            return;
-        //        }
-
-        //    }
-
-        //    void Prompt()
-        //    {
-        //        Console.WriteLine($"Rolled {DiceRolls.Sum()} for {sword.Damage} HP \n \n");
-        //    }
-        //
-        //}
     }
 }
--- SwordDamage_Console/SwordDamage.cs
+++ SwordDamage_Console/SwordDamage.cs
@@ -9,36 +9,85 @@ namespace SwordDamage_Console
 {
     internal class SwordDamage
     {
-        public const int BASE_DAMAGE = 3;
-        public const int FLAME_DAMAGE= 2;
+        private const int BASE_DAMAGE = 3;
+        private const int FLAME_DAMAGE= 2;
 
-        public int Roll;
-        public decimal MagicMultiplier = 1M;
-        public int FlamingDamage = 0;
-        public int Damage;
+        /// <summary>
+        /// Contains the calculated Damage.
+        /// </summary>
+        public int Damage { get; private set; }
 
-        public void CalculateDamage()
+        private int roll;
+
+        /// <summary>
+        /// Set or gets the 3d6 roll;
+        /// </summary>
+        public int Roll
         {
-            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FLAME_DAMAGE;
+            get { return roll; }
+            set { 
+                if(value > 0)
+                    roll = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool flaming;
+
+        /// <summary>
+        /// True if flaming is selected, false otherwise.
+        /// </summary>
+        public bool Flaming
+        {
+            get { return flaming; }
+            set
+            {
+                flaming = value;
+                CalculateDamage();
+            }
+        }
+
+        private bool magic;
+
+        /// <summary>
+        /// True if magic is selected, false otherwise.
+        /// </summary>
+        public bool Magic
+        {
+            get { return magic; }
+            set
+            {
+                magic = value;
+                CalculateDamage();
+            }
+        }
+
+        /// <summary>
+        /// Calculates the damage based on the current properties.
+        /// </summary>
+        private void CalculateDamage()
+        {
+            decimal magicMultiplier = 1M;
+            if (Magic) magicMultiplier = 1.75M;
+
+            Damage = BASE_DAMAGE;
+
+            Damage = (int)(Roll * magicMultiplier) + BASE_DAMAGE + FLAME_DAMAGE;
+            if (Flaming) Damage += FLAME_DAMAGE;
             Debug.WriteLine($"CalculateDamage finished: {Damage} (roll: {Roll})");
+
         }
 
-        public void SetMagic(bool isMagic)
-        {
-            if (isMagic)
-                MagicMultiplier = 1.75M;
-            else
-                MagicMultiplier = 1M;
-            CalculateDamage();
-            Debug.WriteLine($"SetMagic finished: {Damage} (roll: {Roll})");
-        }
+        /// <summary>
+        /// The constructor calculated damage based on default Magic 
+        /// and Flaming values and a starting 3d6 roll.
+        /// </summary>
+        /// <param name="startingRoll">Starting 3d6 roll</param>
 
-        public void SetFlaming(bool isFlaming)
+        private SwordDamage(int startingRoll)
         {
+            roll = startingRoll;
             CalculateDamage();
-            if (isFlaming)
-                Damage += FLAME_DAMAGE;
-            Debug.WriteLine($"SetFlaming finished: {Damage} (roll: {Roll})");
         }
     }
 }
